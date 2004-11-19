b56447e3e689e04706b51331ffae7f58a635162a
waller <waller@informatik.uni-kiel.de>
2004-11-19 11:14:58 +0000

    config.inc.php --> Konfiguration des Servers

---
+++ php1/Project CoMa/include/config.inc.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Konfiguration der ServerParameter
+ *
+ * $Id$
+ *
+ */
+
+$sqlServer = "localhost";
+$sqlUser = "coma1";
+$sqlPassword = "Rhashdeak";
+$sqlDatenbank = "coma1";
+
+$ftpServer = "localhost";
+$ftpUser = "ftp-php1";
+$ftpPassword = "janMiUct";
+
+/**
+ * Diese Funktion sollte nach jedem include dieser Datei so schnell 
+ * wie moeglich nach dem Include dieser Datei ausgefuert werden, 
+ * um eventuellen Passworddiebstahl oder so zu erschweren
+ * 
+ */
+function ConfigClearVariables()
+{
+  unset($sqlPassword);
+  unset($ftpPassword);
+}
+
+?>
