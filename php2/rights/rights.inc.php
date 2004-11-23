2e7466cb49e475920bd196c0cddbff465fc8b2ab
jensen <jensen@informatik.uni-kiel.de>
2004-11-23 15:11:11 +0000

    initially added

---
+++ php2/rights/rights.inc.php
@@ -0,0 +1,11 @@
+<?php
+
+// rights.inc.php
+
+// included to check user access allowance.
+
+// (the realm of the Dziuk)
+
+// at the moment does nothing, so everybody is allowed to do everything.
+
+?>
