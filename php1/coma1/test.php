cff7808a495b118eaedad1b5b8a4ecc8331004bd
waller <waller@informatik.uni-kiel.de>
2004-11-24 20:33:35 +0000

    PHPdoc die x.

--- php1/coma1/test.php
+++ php1/coma1/test.php
@@ -1,8 +1,18 @@
 <?php
-  define('IN_COMA1',true);
+/**
+ * @version $Id$
+ * @package test1
+ */
+
+/**
+ * Wichtig damit Coma1 Datein eingebunden werden koennen
+ *
+ * @ignore
+ */
+define('IN_COMA1',true);
   
-  require('./include/class.mysql.inc.php');
+require('./include/class.mysql.inc.php');
   
-  echo 'Hallo';
+echo 'Hallo';
 
 ?>
--- php1/coma1/test.templates.php
+++ php1/coma1/test.templates.php
@@ -1,6 +1,16 @@
 <?php
+/**
+ * @version $Id$
+ * @package test1
+ */
 
+/**
+ * Wichtig damit Coma1 Datein eingebunden werden koennen
+ *
+ * @ignore
+ */
 define('IN_COMA1',true);
+
 require_once('./include/class.template.inc.php');
 
 $myTemplate = new Template('./templates/test.tpl');
