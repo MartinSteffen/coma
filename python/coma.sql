70e52d8180c8461e922b72b154b5f190923434f9
mky <mky@ifi.uio.no>
2004-11-24 21:50:01 +0000

    Corrections, simplifications, and more constraints.

--- python/coma.sql
+++ python/coma.sql
@@ -2,29 +2,69 @@
 -- model, we define our own one here.  I do not know whether this is
 -- suitable for MySQL as I wrote it for PostgreSQL.
 --
+
+
+
+
+-- The description is better put onto the home page.  We are only concerned
+-- with paper submission.
+--
+-- minimum number of reviews defaults to 1 and may never be null.
+--
 CREATE TABLE Conferences (
     abbreviation                 varchar(20) primary key,
     name                         varchar(127) not null,
-    description                  Text,
-    homepage                     varchar(127),
-    abstract_submission_deadline date,
-    paper_submission_deadline    date,
-    review_deadline              date,
-    notification_deadline        date,
-    final_version_deadline       date,
-    conference_start             date,
-    conference_end               date,
-    min_reviews_per_paper        smallint
+    homepage                     varchar(127) not null,
+    abstract_submission_deadline date null,
+    paper_submission_deadline    date not null,
+    review_deadline              date not null,
+    notification_deadline        date not null,
+    final_version_deadline       date not null,
+    conference_start             date not null,
+    conference_end               date not null,
+    min_reviews_per_paper        smallint not null default 1
 );
+
+
+
+
+
+CREATE TABLE Topics (
+    name                         varchar(127) primary key,
+    conference                   varchar(20) references Conferences
+);
+
+
+
+
+
+-- ---------------------------------------------------------------------------
+-- Users
+--
+-- roles is a bit-vector, where index 
+-- 1 identifies the user as admin
+-- 2 identifies the user as chair
+-- 3 identifies the user as reviewer
+-- 4 identifies the user as author
+--
+-- title is a number, where
+-- 0 is Mr
+-- 1 is Ms
+-- 2 is Mrs
+-- 3 is Dr
+-- 4 is Prof
+-- 5 is Prof Dr
+--
+-- ---------------------------------------------------------------------------
 
 CREATE TABLE Users (
     email                        varchar(127) primary key,
-    conference                   varchar(20),
-    roles
-    first_name                   varchar(20),
+    conference                   varchar(20) references Conferences,
+    roles			 bit(4) not null,
+    first_name                   varchar(20) not null,
     last_name                    varchar(20),
-    title                        Title,
-    affiliation                  varchar(127),
+    title                        smallint not null,
+    affiliation                  varchar(127) not null,
     phone_number                 varchar(20),
     fax_number                   varchar(20),
     street                       varchar(127),
@@ -32,27 +72,39 @@ CREATE TABLE Users (
     city                         varchar(127),
     state                        varchar(127),
     country                      varchar(127),
-    password                     varchar(20)
+    password                     varchar(20) not null
 );
+
+
+
+
 
 -- -------------------------------------------------------------------------
 -- Fora.
 -- -------------------------------------------------------------------------
 
+CREATE TABLE Forum (
+    id                            serial primary key,
+    conference                    varchar(20) references Conference,
+    title                         varchar(127) not null,
+    description                   text not null
+);
+
+
+
+
+
 CREATE TABLE Message (
     id                            varchar(127) primary key,
     in_reply_to                   varchar(127) null,
-    send_time                     timestamp,
-    subject                       varchar(127),
-    contents                      text
+    send_time                     timestamp not null,
+    subject                       varchar(127) not null,
+    contents                      text not null
 );
 
-CREATE TABLE Forum (
-    id                            integer autoincrement primary key,
-    conference                    varchar(20) references Conference,
-    title                         varchar(127) unique not null,
-    description                   text
-);
+
+
+
 
 CREATE TABLE Threads (
     forum_id                      integer references Forum,
@@ -60,51 +112,93 @@ CREATE TABLE Threads (
     UNIQUE(forum_id, message_id)
 );
 
+
+
+
+
 -- ---------------------------------------------------------------------------
 -- Papers
+--
+-- The abstract of a paper is the description inherited from the forum
 -- ---------------------------------------------------------------------------
 
 CREATE TABLE Papers (
-    contactauthor                varchar(127), -- references Users
-    abstract                     Text,
-    filename                     varchar(127) unique,
-    state                        integer,
-    format                       integer
+    contactauthor                varchar(127) references Users,
+    filename                     varchar(127) unique not null,
+    state                        smallint not null,
+    format                       smallint not null
 ) INHERITS(Forum);
 
+
+
+
+
 CREATE TABLE CoAuthors (
-    conference                   varchar(20),
+    conference                   varchar(20) references Conferences,
     paper_id                     integer references Papers,
-    name                         varchar(127)
+    first_name                   varchar(127) not null,
+    last_name                    varchar(127) not null
 );
 
-CREATE TABLE Topics (
-    name                         varchar(127) primary key,
-    conference                   varchar(20) references Conference
-);
 
-CREATE TABLE AboutTopic (
+
+
+
+
+-- Associate topics to papers
+CREATE TABLE AboutTopics (
     paper_id                     integer     references Papers,
-    topic                        varchar(20) references Topics
+    topic                        varchar(20) references Topics,
+    UNIQUE(paper_id, topic)
 );
 
-CREATE TABLE PrefersTopic (
+
+
+
+
+-- Associate topics to reviewers
+CREATE TABLE PreferedTopics (
     email                        varchar(127) references Users,
     topic                        varchar(127) references Topics,
     UNIQUE(email, topic)
 );
 
-CREATE TABLE PrefersPaper (
+
+
+
+
+-- Associate papers to reviewers
+CREATE TABLE PreferedPapers (
     email                        varchar(127) references Users,
     paper_id                     integer references Papers,
     UNIQUE(email, paper_id)
 );
 
-CREATE TABLE ExcludedPaper (
+
+
+
+
+-- Associate papers to reviewers
+CREATE TABLE ExcludedPapers (
     email                        varchar(127) references Users,
     paper_id                     integer references Papers,
     UNIQUE(email, paper_id)
 );
+
+
+
+
+
+-- Associate papers to reviewers
+CREATE TABLE DeniedPapers (
+    email                        varchar(127) references Users,
+    paper_id                     integer references Papers,
+    UNIQUE(email, paper_id)
+);
+
+
+
+
 
 -- ---------------------------------------------------------------------------
 -- Review reports.
