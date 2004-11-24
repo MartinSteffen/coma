e4a0035af38bdf0cdec54b9c227517b42afc1233
waller <waller@informatik.uni-kiel.de>
2004-11-24 09:04:19 +0000

    Templates improved

--- php1/coma1/include/class.template.inc.php
+++ php1/coma1/include/class.template.inc.php
@@ -1,38 +1,46 @@
 <?php
 
+if (!defined('IN_COMA1')) {
+  exit('Hacking attempt');
+}
+
 /**
  * Klasse zum Parsen der Templates
  *
  * $Id$
  *
  */
-
-if (!defined('IN_COMA1')) {
-  exit('Hacking attempt');
-}
-
 class Template {
 
   var $template='';
   var $errString = '';
+  var $assocArray = array();
 
-  function Template() {
-    return true;
+  function Template($filename) {
+    return readTemplate($filename);
   }
   
-  function readTemplate($template) {
+  function readTemplate($filename) {
     // vor PHP 4.3 waere es das gewesen:
-    // $contents = implode("", @file($template) );
-    $contents = file_get_contents($template);
+    // $contents = implode("", @file($filename) );
+    $contents = file_get_contents($filename);
     if (empty($contents)) {
-      return $this->error("Could not read Template [$template]");
+      return $this->error("Could not read Template [$filename]");
     }
     $this->template = $contents;
     return true;
   }
   
-  function parse($assocArray) {
+  function assign($assocArray) {
+    if(!is_array($assocArray)) {
+      return $this->error('Not an Array');
+    }
+    $this->assocArray = array_merge($this->assocArray, $assocArray);
+  }
+  
+  function parse() {
     $template = $this->template;
+    $assocArray = $this->assocArray;
     $keyArray = array_keys($assocArray);
     $keyArray = array_map(create_function('$s', 'return "<{" . $s . "}>";'), $keyArray);
     $template = preg_replace($keyArray, array_values($assocArray), $template);
--- php1/coma1/test.templates.php
+++ php1/coma1/test.templates.php
@@ -3,15 +3,16 @@
 define('IN_COMA1',true);
 require_once('./include/class.template.inc.php');
 
-$myTemplate = new Template();
-
-$myTemplate->readTemplate('./templates/test.tpl');
+$myTemplate = new Template('./templates/test.tpl');
 
 $assocArray = array(
   'tag1' => 'Hallo',
   'tag2' => 'Test'
 );
 
-echo $myTemplate->parse($assocArray);
+$myTemplate->assign($assocArray);
+
+echo $myTemplate->parse();
+
 
 ?>
