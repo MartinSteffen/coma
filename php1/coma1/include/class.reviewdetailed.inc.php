83a46dc49a861a3ecd2cfdccd4661dc5ba854f05
esquivel <esquivel@informatik.uni-kiel.de>
2004-11-25 09:58:40 +0000

    Klasse ReviewDetailed hinzugefuegt.

---
+++ php1/coma1/include/class.reviewdetailed.inc.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * @version $Id: class.review.inc.php 164 2004-11-25 08:10:39Z waller $
+ * @package coma1
+ */
+/***/
+if (!defined('IN_COMA1')) {
+  exit('Hacking attempt');
+}
+
+require_once('./class.review.inc.php');
+
+/**
+ * Klasse ReviewDetailed
+ *
+ * @author  Jan Waller <jwa@informatik.uni-kiel.de>
+ * @copyright Copyright (c) 2004, Jan Waller
+ * @package coma1
+ * @subpackage Objects
+ * @access public
+ *
+ */
+class ReviewDetailed extends Review {
+
+} // end class ReviewDetailed
+
+?>
