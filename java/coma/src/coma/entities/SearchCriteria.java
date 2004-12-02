8313a4ea68fecc275675c9d312897ca451ca283d
albari <albari@informatik.uni-kiel.de>
2004-12-02 23:28:52 +0000

    added db-interface

---
+++ java/coma/src/coma/entities/SearchCriteria.java
@@ -0,0 +1,14 @@
+
+package coma.entities;
+
+/**
+ * @author <a href="mailto:mal@informatik.uni-kiel.de">Mohamed Albari</a>
+ * Created on Dec 2, 2004  11:03:07 PM
+ */
+
+public class SearchCriteria {
+    
+    public SearchCriteria(){
+    }
+    
+}
