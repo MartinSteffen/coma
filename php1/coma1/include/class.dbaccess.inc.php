d86d9c9850066505c67ece3d0b24fbf419bcf39c
scherzer <scherzer@informatik.uni-kiel.de>
2004-12-04 17:20:19 +0000

    DBAccess und PaperDetailed ein wenig korrigiert und ueberarbeitet.

--- php1/coma1/include/class.dbaccess.inc.php
+++ php1/coma1/include/class.dbaccess.inc.php
@@ -33,11 +33,9 @@ class DBAccess {
   /**#@-*/
 
   /**
-   * Konstruktor
+   * Konstruktor: Erzeugt eine Verbindung mit der Datenbank.
    *
-   * Der Konstruktor erzeugt eine Verbindung mit der Datenbank.
-   *
-   * @param MySql $mySql Ein MySql-Objekt
+   * @param MySql $mySql ein MySql-Objekt
    * @return bool <b>true</b> bei Erfolg, <b>false</b> bei Fehler
    * @see error()
    * @see getLastError()
@@ -48,10 +46,10 @@ class DBAccess {
   }
 
   /**
-   * Fehlerbeschreibung festlegen
+   * Fehlerbeschreibung festlegen.
    *
-   * @param string $strError Optionale Fehlerbeschreibung.
-   * @return false Es wird immer <b>false</b> zurueckgegeben.
+   * @param string $strError optionale Fehlerbeschreibung
+   * @return false immer <b>false</b>
    * @see getLastError()
    * @access protected
    */
@@ -61,12 +59,12 @@ class DBAccess {
   }
 
   /**
-   * Letzten Fehler zurueckgeben
+   * Letzten Fehler zurueckgeben.
    *
    * Die Funktion <b>getLastError()</b> gibt die letzte mit error
    * gesicherte Fehlermeldung zurueck und loescht diese aus dem Speicher.
    *
-   * @return string Die letzte Fehlermeldung.
+   * @return string die letzte Fehlermeldung
    * @see error()
    * @access public
    */
@@ -96,6 +94,11 @@ class DBAccess {
   }
 
   /**
+   * Liefert die ID der Person, deren E-Mail-Adresse $strEmail ist.
+   *
+   * @param string $strEmail E-Mail-Adresse der Person
+   * @return int ID bzw. <b>false</b>, falls keine Person mit E-Mail-Adresse $strEmail gefunden wurde
+   * @access public
    */
   function getPersonIdByEmail($strEmail) {
     $s = 'SELECT  id'.
@@ -109,6 +112,11 @@ class DBAccess {
   }
   
   /**
+   * Liefert ein Person-Objekt mit den Daten der Person $intPersonId.
+   *
+   * @param int $intPersonId ID der Person
+   * @return Person bzw. <b>false</b>, falls keine Person mit ID $intPersonId gefunden wurde
+   * @access public
    */
   function getPerson($intPersonId) {
     $s = 'SELECT  id, email, first_name, last_name'.
@@ -177,7 +185,11 @@ class DBAccess {
   /**
    */
   function getPaperDetailed($intPaperId) {
-    return true;
+/*    $s = 'SELECT  id, conference_id, author_id, title, abstract, last_edited,'.
+        '         version, filename, state, mime_type'.
+        ' FROM    Paper'.
+        ' WHERE   id = '.$intPersonId;*/
+    return false;
   }
 
   /**
--- php1/coma1/include/class.paperdetailed.inc.php
+++ php1/coma1/include/class.paperdetailed.inc.php
@@ -23,24 +23,25 @@ require_once('./class.papersimple.inc.php');
  */
 class PaperDetailed extends PaperSimple {
 
-  var $intCoAuthorsIds;
-  var $strCpAuthors;
+  var $intCoAuthorIds;
+  var $strCoAuthors;
   var $strAbstract;
   var $intFormatId;
   var $strLastEdit;
   var $strFilePath;
 
-  function PaperDetailed($id, $title, $authorId, $status, $avgRating = 0.0, $coAuthorsIds='', $cpAuthors, $abstract, $formatId, $lastEdit, $filePath){
+  function PaperDetailed($id, $title, $authorId, $status, $avgRating = 0.0, $coAuthorIds='', $coAuthors, $abstract, $formatId, $lastEdit, $filePath){
     $this->PaperSimple($id, $title, $authorId, $status, $avgRating);
-    $this->intCoAuthorsIds = $coAuthorsIds;
-    $this->strCpAuthors = $cpAuthors;
+    $this->intCoAuthorsIds = $coAuthorIds;
+    $this->strCoAuthors = $coAuthors;
     $this->strAbstract = $abstract;
     $this->intFormatId = $formatId;
     $this->strLastEdit = $lastEdit;
     $this->strFilePath = $filePath;
   }
 
-  function PaperDetailedFromPaperSimple($paperSimple, $coAuthorsIds='', $cpAuthors, $abstract, $formatId, $lastEdit, $filePath){
+  // weg damit! (Tom)
+  function PaperDetailedFromPaperSimple($paperSimple, $coAuthorIds='', $coAuthors, $abstract, $formatId, $lastEdit, $filePath){
     $this->PaperDetailed($paperSimple->intId, $paperSimple->strTitle, $paperSimple->intAuthorId, $paperSimple->intStatus, $paperSimple->fltAvgRating, $coAuthorsIds, $cpAuthors, $abstract, $formatId, $lastEdit, $filePath);
   }
 
