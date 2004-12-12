b93db69c127af51ab0530401382e769889dcd6c6
scherzer <scherzer@informatik.uni-kiel.de>
2004-12-12 19:52:43 +0000

    Da ist sie wieder...

---
+++ php1/coma1/reviewer.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * @version $Id$
+ * @package coma1
+ * @subpackage core
+ */
+/***/
+
+/**
+ * Wichtig damit Coma1 Dateien eingebunden werden koennen
+ *
+ * @ignore
+ */
+define('IN_COMA1',true);
+
+require_once('./include/header.inc.php');
+
+// Haupt-Ansicht fuer den Reviewer
+$mainPage = new Template(TPLPATH.'main.tpl');
+$menue = new Template(TPLPATH.'nav_reviewer.tpl');
+
+
+$strMainAssocs = defaultAssocArray();
+$strMainAssocs['titel'] = 'Chair-Verwaltung';
+$strMainAssocs['content'] = 'Eingeloggt! als: ' . $_SESSION['uname'];
+$strMainAssocs['menue'] =& $menue;
+$strMainAssocs['submenue'] = '';
+$strMainAssocs['body'] = '';
+$mainPage->assign($strMainAssocs);
+
+$menue->assign(defaultAssocArray());
+
+$mainPage->parse();
+$mainPage->output();
+
+?>
