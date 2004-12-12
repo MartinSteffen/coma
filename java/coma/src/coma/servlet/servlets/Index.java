aae03a8fc12ccf83a2188fff571137d70a62d844
tiedje <tiedje@informatik.uni-kiel.de>
2004-12-12 12:31:38 +0000

    First servlet invoked by the browser: add to web.xml something like this: <servlet-mapping>    <servlet-name>Index</servlet-name>    <url-pattern>/index.html</url-pattern> </servlet-mapping>

---
+++ java/coma/src/coma/servlet/servlets/Index.java
@@ -0,0 +1,56 @@
+/*
+ * Created on 10.12.2004
+ *
+ */
+package coma.servlet.servlets;
+
+import java.io.PrintWriter;
+import java.io.StringReader;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.xml.transform.stream.StreamSource;
+
+import coma.servlet.util.XMLHelper;
+
+/**
+ * @author mti
+ *
+ * First servlet invoked by the browser
+ * No function, maybe later 
+ *
+ */
+public class Index  extends HttpServlet {
+	
+	
+	
+	
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+    	throws ServletException, java.io.IOException {
+		
+		StringBuffer result = new StringBuffer();	
+		XMLHelper helper = new XMLHelper();
+		
+		String path = getServletContext().getRealPath("");
+		String xslt = path+"/style/xsl/index.xsl";
+		PrintWriter out = response.getWriter();
+		helper.addXMLHead(result);
+		result.append("<status>\n");
+		result.append("</status>\n");
+		response.setContentType("text/html; charset=ISO-8859-15");
+		StreamSource xmlSource = new StreamSource(new StringReader(result.toString()));
+		StreamSource xsltSource = new StreamSource(xslt);
+		XMLHelper.process(xmlSource, xsltSource, out);
+		out.flush();
+		
+		
+	
+	
+	}
+	
+	public void doPost(HttpServletRequest request, HttpServletResponse response)
+    	throws ServletException,java.io.IOException{
+		doGet(request, response);
+	}
+}
