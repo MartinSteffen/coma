c9621539d1f31ebbaa0b5084427024eb0ac2bd98
waller <waller@informatik.uni-kiel.de>
2005-01-11 18:15:39 +0000

    new Layout Test

---
+++ php1/coma1/templates/coma-3/startmenu.php
@@ -0,0 +1,13 @@
+<?php
+function openStartMenuItem ($intItem) {
+  $menu = new Template(TPLPATH.'startmenu.tpl');
+  $strMenuAssocs = defaultAssocArray();
+  for ($i = 1; $i <= 4; $i++) {
+    $strMenuAssocs['marked-'.$i] = '';
+  }
+  $strMenuAssocs['marked-'.$intItem] = ' class="markedmenuitem"';
+  $menu->assign($strMenuAssocs);
+  $menu->parse();
+  return $menu->strOutput;
+}
+?>
