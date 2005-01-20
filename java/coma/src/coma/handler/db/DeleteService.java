9a5eb2f32d39ad9035d289bd525f4a7f1f964672
albari <albari@informatik.uni-kiel.de>
2005-01-20 09:30:54 +0000

    added

---
+++ java/coma/src/coma/handler/db/DeleteService.java
@@ -0,0 +1,18 @@
+package coma.handler.db;
+
+import coma.entities.SearchResult;
+
+/**
+ * Created on 19.01.2005
+ * <a href="mailto:mal@informatik.uni-kiel.de">Mohamed Z. Albari</a>
+ */
+
+public interface DeleteService {
+	
+	public SearchResult deleteConference(int conference_id);
+	public SearchResult deletePerson(int person_id);
+	public SearchResult deletePaper(int paper_id);
+	public SearchResult deleteCriterion(int criterion_id);
+	public SearchResult deleteReviewReport(int report_id);
+	public SearchResult deleteTopic(int topic_id);
+}
