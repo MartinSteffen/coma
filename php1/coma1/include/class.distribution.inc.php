37dacd68bdaae6d360fa8546bb8181ff6da77e7c
scherzer <scherzer@informatik.uni-kiel.de>
2005-01-21 00:53:27 +0000

    Distribution, erste Vorbereitungen

---
+++ php1/coma1/include/class.distribution.inc.php
@@ -0,0 +1,83 @@
+<?php
+/**
+ * @version $Id:
+ * @package coma1
+ * @subpackage core
+ */
+/***/
+if (!defined('IN_COMA1')) {
+  die('Hacking attempt');
+}
+
+if (!defined('INCPATH')) {
+  /** @ignore */
+  define('INCPATH', dirname(__FILE__).'/');
+}
+
+require_once(INCPATH.'header.inc.php');
+
+class Distribution extends ErrorHandling {
+  /**#@+
+   * @access private
+   */
+  /**
+   * @var MySql
+   */
+  var $mySql;
+  /**#@-*/
+
+  /**
+   * Konstruktor: Erzeugt eine Verbindung mit der Datenbank.
+   *
+   * @param MySql $mySql ein MySql-Objekt
+   * @return bool true gdw. erfolgreich
+   * @see error()
+   * @see getLastError()
+   */
+  function Distribution(&$mySql) {
+    $this->mySql = &$mySql;
+    return $this->success();
+  }
+
+  /**
+   * Liefert ein zweidimensionales Array von Paper/Reviewer-Zuordnungen von den
+   * Papern, die noch nicht in der Distribution-Tabelle auftauchen. Um einem
+   * bereits vorher zugeordneten Paper weitere Reviewer hinzuzufuegen, siehe
+   * andere Funktion.
+   *
+   * Benutzt (als Bedingungen):
+   * - Distribution-Tabelle (Basis)
+   *
+   * @return int [] Das (ggf. leere) Array mit einem Verteilungsvorschlag bzw. false,
+   *                wenn keine gueltige Verteilung existiert oder keine Konferenz
+   *                angegeben wird.
+   *                Jeder Arrayeintrag enthaelt ein Array mit den Indizes
+   *                'intPaperId' und 'intReviewerId'.
+   *                Beispiel: $x = getDistribution(); echo($x[$i]['intPaperId']);
+   *
+   * @access public
+   * @author Falk, Tom (20.01.05)
+   */
+  function getDistribution($intConferenceId) {
+    if (empty($intConferenceId)) {
+      return $this->success(false);
+    }
+    $s = sprintf("SELECT   p.id AS paper_id, p.title AS title".
+                 " FROM    Paper AS p".
+                 " LEFT    JOIN Distribution AS d".
+                 " ON      d.paper_id = p.id"
+                 " WHERE   p.conference_id = '%s'".
+                 " AND     d.paper_id IS NULL",
+                 s2db($intConferenceId));
+    $data = $this->mySql->select($s);
+    if ($this->mySql->failed()) {
+      return $this->error('getDistribution', $this->mySql->getLastError());
+    }
+    echo('<br>Unverteilte Paper:<br>');
+    for ($i = 0; $i < count($data); $i++) {
+      echo('<br>'.$data[$i]['paper_id'].' / '.$data[$i]['title'].);
+    }
+    return false;
+  }
+
+}
---
+++ php1/coma1/test_distribution.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * @version $Id
+ * @package coma1
+ * @subpackage Testing
+ */
+/***/
+
+/**
+ * Wichtig damit Coma1 Dateien eingebunden werden koennen
+ *
+ * @ignore
+ */
+
+define('IN_COMA1', true);
+define('NEED_NO_LOGIN', true);
+
+require_once('./include/header.inc.php');
+require_once('./include/class.getdistribution.inc.php');
+
+function bit($b) {
+  for ($i = 15; $i >= 0; $i--) {
+    echo(($b & (1 << $i)) ? '1':'0');
+  }
+  echo('<br>');
+  return true;
+}
+
+echo('<br>');
+
+
+$myDist = new Distribution($mySql);
+
+$myDist->getDistribution();
+if ($myDist->failed()) {
+  echo($myDist->getLastError());
+}
+else if (empty($p)) {
+  echo('EMPTY');
+  die(-1);
+}
+echo('OK');
+
+?>
