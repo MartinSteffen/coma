9f0475f2ca163d5e055eb1a8ef9c0d04ccef9583
niemann <niemann@informatik.uni-kiel.de>
2005-01-27 18:05:41 +0000

    NoComment

---
+++ tests/php1/web/phpUnit/index.php
@@ -0,0 +1,22 @@
+<?php
+
+// Required Libraries
+require_once 'PHPUnit.php';
+require_once 'PHPUnit/gui/HTML.php';
+
+// Required Test Classes
+require_once 'testsuites/RequiredFunctions.php';
+require_once 'testsuites/ComaExample.php';
+
+// Build the Suites
+$suites = array();
+
+$suites[] = new PHPUnit_TestSuite("RequiredFunctions");
+$suites[] = new PHPUnit_TestSuite("ComaExample");
+
+// Build the GUI and Run the Suites
+$gui = new PHPUnit_GUI_HTML($suites);
+//$gui->addSuites($suites);
+$gui->show();
+
+?>
