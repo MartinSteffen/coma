db0ddfa3e88007092d38c75823ac175b8f11441d
niemann <niemann@informatik.uni-kiel.de>
2005-01-27 16:05:25 +0000

    NoComment

--- tests/update_php1.sh
+++ tests/update_php1.sh
@@ -1,4 +1,11 @@
 #! /bin/sh
+
+if test -d ~/public_html/php1 ; then
+    svn update ~/public_html/php1
+else
+    svn checkout http://localhost:8080/svn/coma/trunk/tests/web/php1 ~/public_html/php1
+fi 
+
 if test -d ~/public_html/php1/coma1 ; then
     svn update ~/public_html/php1/coma1
 else
