c64bdf275c3d3b65a528f6fb1985d8c8df8a94ba
niemann <niemann@informatik.uni-kiel.de>
2005-01-27 21:42:35 +0000

    NoComment

--- tests/php1/web/phpUnit/testsuites/ComaExample.php
+++ tests/php1/web/phpUnit/testsuites/ComaExample.php
@@ -5,7 +5,7 @@ require_once 'PHPUnit.php';
 // Coma must be installed in ../coma1/
 define('IN_COMA1', true);
 define('NEED_NO_LOGIN', true);
-require_once '../coma1/include/header.inc.php';
+include('../coma1/include/header.inc.php');
 
 $strEmail = "sae@me.de";
 
