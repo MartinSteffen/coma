6edee3e60d9c5749d3b706d807f9d7ff02528ae2
niemann <niemann@informatik.uni-kiel.de>
2005-01-27 21:03:58 +0000

    NoComment

--- tests/php1/web/phpUnit/testsuites/ComaExample.php
+++ tests/php1/web/phpUnit/testsuites/ComaExample.php
@@ -5,7 +5,7 @@ require_once 'PHPUnit.php';
 // Coma must be installed in ../coma1/
 define('IN_COMA1', true);
 define('NEED_NO_LOGIN', true);
-include('../../coma1/include/header.inc.php');
+require_once '../../coma1/include/header.inc.php';
 
 class ComaExample extends PHPUnit_TestCase
 {
