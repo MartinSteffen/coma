b8df79e251732fa1b761b7594b15cbf7358884a1
biederbeck <biederbeck@informatik.uni-kiel.de>
2005-01-28 18:22:13 +0000

    Integration Chair - Forum

--- php2/modules/forum/newforum/sub_default.inc.php
+++ php2/modules/forum/newforum/sub_default.inc.php
@@ -6,8 +6,9 @@ if (isChair_Overall())
 {
 	$SQL = "INSERT INTO forum VALUES (0, ".$_POST['confID'].", '".$_POST['title']."', ".$_POST['type'].", DEFAULT)";
 
-	// $sql->insert($SQL);
+	$sql->insert($SQL);
 
-	template("FORUM_viewforum");
+	redirect('chair','conferences', false, false);
+
 } else redirect(false, false, false, false);
 ?>
